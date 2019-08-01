c0a1e3921e3f24f8caac3d2cedbbdd177d255fc1
Dave <davidgrace00@gmail.com>
2019-08-01 23:27:35 +0100

    Remove unnecessary using

--- RoundTheCode.LinqExpressions-Example.Expressions/AndOrExpression.cs
+++ RoundTheCode.LinqExpressions-Example.Expressions/AndOrExpression.cs
@@ -1,10 +1,8 @@
 ﻿using RoundTheCode.LinqExpressions_Example.Data;
 using RoundTheCode.LinqExpressions_Example.Data.Models.Types;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace RoundTheCode.LinqExpressions_Example.Expressions
 {
